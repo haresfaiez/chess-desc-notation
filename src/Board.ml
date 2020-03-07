d23a05fe99d7e4dfb9a9835e1e35b3bc28d41bb1
faiez <felhares@iobeya.com>
2020-03-07 14:12:57 +0100

    Put pawns in all files of the second rank

--- src/Board.ml
+++ src/Board.ml
@@ -2,10 +2,12 @@ module Board = struct
   type piece =
     | King
     | Queen
+    | Pawn
 
   let occupant file rank =
     match rank with
     | 1 -> Some file
+    | 2 -> Some Pawn
     | _ -> None
 end
 
--- src/Board.mli
+++ src/Board.mli
@@ -2,5 +2,6 @@ module Board : sig
   type piece =
     | King
     | Queen
+    | Pawn
   val occupant: piece -> int -> piece option
 end
