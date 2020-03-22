04970a934bf663d91f92e90ef512b4e071b22124
faiez <felhares@iobeya.com>
2020-03-22 10:44:58 +0100

    use `occupant (piece * rank)` to test occupant of a given in-board position

--- __tests__/Board_test.ml
+++ __tests__/Board_test.ml
@@ -8,11 +8,11 @@ let () =
       test "puts the white king in K1" (fun () ->
           expect (Board._occupant (Board.King, (Board.Rank 1))) |> toBe (Some Board.King));
       test "puts the white queen in Q1" (fun () ->
-          expect (Board.occupant (Board.Queen, 1)) |> toBe (Some Board.Queen));
+          expect (Board._occupant (Board.Queen, (Board.Rank 1))) |> toBe (Some Board.Queen));
       test "puts no piece in K3" (fun () ->
-          expect (Board.occupant (Board.King, 3)) |> toBe None);
+          expect (Board._occupant (Board.King, (Board.Rank 3))) |> toBe None);
       test "puts no piece in Q5" (fun () ->
-          expect (Board.occupant (Board.Queen, 5)) |> toBe None);
+          expect (Board._occupant (Board.Queen, (Board.Rank 5))) |> toBe None);
       test "puts no piece above the board" (fun () ->
           expect (Board.occupant (Board.Queen, 9)) |> toBe None);
       test "puts no piece below the board" (fun () ->
