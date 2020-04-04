fe04994e14209a7fb43f41a2913b9da7642c29a8
faiez <felhares@iobeya.com>
2020-04-04 11:52:57 +0100

    replace king position variables with a call to helper in tests

--- __tests__/Board_test.ml
+++ __tests__/Board_test.ml
@@ -5,11 +5,6 @@ open Board
 
 let k rank : Board.position = (Board.King, (Board.Rank rank))
 
-let k1 : Board.position = k 1
-let k7 : Board.position = (Board.King, (Board.Rank 7))
-let k2 : Board.position = (Board.King, (Board.Rank 2))
-let k3 : Board.position = (Board.King, (Board.Rank 3))
-
 let q1 : Board.position = (Board.Queen, (Board.Rank 1))
 let q5 : Board.position = (Board.Queen, (Board.Rank 5))
 
@@ -22,23 +17,23 @@ let () =
       test "puts the white queen in Q1" (fun () ->
           expect (Board.occupant q1) |> toBe (Some Board.Queen));
       test "puts no piece in K3" (fun () ->
-          expect (Board.occupant k3) |> toBe None);
+          expect (Board.occupant (k 3)) |> toBe None);
       test "puts no piece in Q5" (fun () ->
           expect (Board.occupant q5) |> toBe None);
       test "third rank is empty" (fun () ->
-          expect (Board.occupant k3) |> toBe None);
+          expect (Board.occupant (k 3)) |> toBe None);
     );
   describe "Initial pawn movement" (fun () ->
       test "fails when the destination is K7" (fun () ->
-          expect (Board.move Board.Pawn (k7, None)) |> toBe Board.NoPieceToMove);
+          expect (Board.move Board.Pawn ((k 7), None)) |> toBe Board.NoPieceToMove);
       test "fails when moved backward" (fun () ->
           expect (Board.move Board.Pawn ((k 1), None)) |> toBe Board.NoPieceToMove);
     );
   describe "Initial knight movement" (fun () ->
       test "fails when the destination is K7" (fun () ->
-          expect (Board.move Board.Knight (k7, None)) |> toBe Board.NoPieceToMove);
+          expect (Board.move Board.Knight ((k 7), None)) |> toBe Board.NoPieceToMove);
     );
   describe "Initial King movement" (fun () ->
       test "fails when the destination is K2" (fun () ->
-          expect (Board.move Board.King (k2, (Some Board.Pawn))) |> toBe Board.OccupiedDestination);
+          expect (Board.move Board.King ((k 2), (Some Board.Pawn))) |> toBe Board.OccupiedDestination);
     );
