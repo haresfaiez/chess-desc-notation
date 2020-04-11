ace2fc36b77e048def4dca54751947afbefdffe0
faiez <felhares@iobeya.com>
2020-04-11 12:58:29 +0100

    Remove source argument from Board.move

--- src/Board.ml
+++ src/Board.ml
@@ -30,7 +30,7 @@ module Board = struct
     | Some piece -> Occupied (position, piece)
     | _          -> Empty position
 
-  let move piece source destination =
+  let move piece destination =
     match destination with
     | (King, (Rank 3)) -> Moved
     | _                -> Unreachable
@@ -38,7 +38,7 @@ module Board = struct
   let turn piece destination =
     match destination with
     | Occupied _      -> OccupiedDestination
-    | Empty position  -> move piece [] position
+    | Empty position  -> move piece position
 
   let rec play moves =
     match moves with
