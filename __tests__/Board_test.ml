c56d3478c92a804da90265265f24c8a32b9fa3aa
faiez <felhares@iobeya.com>
2020-05-03 11:41:01 +0100

    Use moveP to create pawn movements in test

--- __tests__/Board_test.ml
+++ __tests__/Board_test.ml
@@ -22,7 +22,7 @@ let () =
       test "-K7 is unreachable from K2" (fun () ->
           expect (Board._move Board.Pawn [(k 2)] (k 7)) |> toBe Board.Unreachable);
       test "-K3 succeeds from K2" (fun () ->
-          expect (Board._move Board.Pawn [(k 2)] (k 3)) |> toEqual (Board.Moved (Board.Pawn, (k 2), (k 3))));
+          expect (Board._move Board.Pawn [(k 2)] (k 3)) |> toEqual (Board.Moved (moveP (k 2) 1)));
     );
   describe "Initial movement: K" (fun () ->
       test "-K3 fails" (fun () ->
