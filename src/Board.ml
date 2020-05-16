aa73ae1314677fd23e21335a08bed4999fb2074f
faiez <felhares@iobeya.com>
2020-05-16 10:18:11 +0100

    Search in source options for destination when options are many

--- src/Board.ml
+++ src/Board.ml
@@ -34,7 +34,9 @@ module Board = struct
     | Pawn   -> [((Knight, Rank 2), []); ((Queen, Rank 2), []); ((King, Rank 2), []); ((Knight, Rank 2), [])]
     | _      -> [((piece, Rank 1), []); ((piece, Rank 1), [])]
 
-  let isDestination (source, options) destination = options = [destination]
+  let isDestination (source, options) destination =
+    let isEqual each = each = destination in
+    List.exists isEqual options
 
   let rec moveOptions piece sources destination =
     let check (piece, (source, _), destination) next = (* Replace with source.options contains destination *)
