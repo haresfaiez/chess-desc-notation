9cda89a234766abb5285b307247a04a20c5b3656
faiez <felhares@iobeya.com>
2020-05-17 09:22:15 +0100

    Inline destination variable in shiftRank

--- __tests__/Board_test.ml
+++ __tests__/Board_test.ml
@@ -4,9 +4,7 @@ open Expect
 open Board
 
 let shift file origin steps : Board.position = (file, Board.Rank (origin + steps))
-let shiftRank piece (file, Board.Rank origin) steps =
-  let destination = shift file origin steps in
-  (piece, (file, Board.Rank origin), destination)
+let shiftRank piece (file, Board.Rank origin) steps = (piece, (file, Board.Rank origin), (shift file origin steps))
 let k rank           : Board.position = (Board.King, (Board.Rank rank))
 let q rank           : Board.position = (Board.Queen, (Board.Rank rank))
 let kn rank          : Board.position = (Board.Knight, (Board.Rank rank))
