56b6c1a9319d763fde95228b9094448247898895
faiez <felhares@iobeya.com>
2020-05-24 11:26:26 +0100

    Remove underscore from test helper names

--- __tests__/Board_test.ml
+++ __tests__/Board_test.ml
@@ -8,8 +8,8 @@ let shiftRank piece source steps = (piece, source, shift source steps)
 let k rank             = (Board.King, (Board.Rank rank))
 let q rank             = (Board.Queen, (Board.Rank rank))
 let kn rank            = (Board.Knight, (Board.Rank rank))
-let _moveP from count  = let (p, s, d) = shiftRank Board.Pawn from count in (p, (s, []), d)
-let _moveQ from count  = let (p, s, d) = shiftRank Board.Queen from count in (p, (s, []), d)
+let moveP from count  = let (p, s, d) = shiftRank Board.Pawn from count in (p, (s, []), d)
+let moveQ from count  = let (p, s, d) = shiftRank Board.Queen from count in (p, (s, []), d)
 let source position    = (position, [])
 
 let destination actual = match actual with | Board.Moved (_, _, result) -> result
@@ -52,19 +52,19 @@ let () =
     );
   describe "PlayTurn" (fun () ->
       test "detects no conflicts in [P-K3, P-Q3]" (fun () ->
-        expect (destination (Board.turn Board.Pawn (q 3) [(_moveP (k 2) 1)])) |> toEqual (q 3));
+        expect (destination (Board.turn Board.Pawn (q 3) [(moveP (k 2) 1)])) |> toEqual (q 3));
       test "detects a conflict in [P-Q3, Q-Q3]" (fun () ->
-        expect (Board.turn Board.Queen (q 3) [(_moveP (q 2) 1)]) |> toBe Board.Conflict);
+        expect (Board.turn Board.Queen (q 3) [(moveP (q 2) 1)]) |> toBe Board.Conflict);
       test "detects a conflict in [P-Q3, P-K3, Q-Q3]" (fun () ->
-        expect (Board.turn Board.Queen (q 3) [(_moveP (k 2) 1); (_moveP (q 2) 1)]) |> toBe Board.Conflict);
+        expect (Board.turn Board.Queen (q 3) [(moveP (k 2) 1); (moveP (q 2) 1)]) |> toBe Board.Conflict);
       test "detects no conflicts in [P-K3]" (fun () ->
         expect (destination (Board.turn Board.Pawn (k 3) [])) |> toEqual (k 3));
       test "detects a conflict at Q-Q4 in [P-Q3, P-Q4, Q-Q4]" (fun () ->
-        expect (Board.turn Board.Queen (q 4) [(_moveP (q 3) 1); (_moveP (q 2) 1)]) |> toBe Board.Conflict);
+        expect (Board.turn Board.Queen (q 4) [(moveP (q 3) 1); (moveP (q 2) 1)]) |> toBe Board.Conflict);
       test "detects no conflicts in [Q-Q2, Q-Q1, Q-Q2, Q-Q1]" (fun () ->
-        expect (destination (Board.turn Board.Queen (q 1) [(_moveQ (q 1) 1); (_moveQ (q 2) (-1))])) |> toEqual (q 1));
+        expect (destination (Board.turn Board.Queen (q 1) [(moveQ (q 1) 1); (moveQ (q 2) (-1))])) |> toEqual (q 1));
       test "detects unreachable destination in [P-Kn3, Kn-Kn2]" (fun () ->
-        expect (Board.turn Board.Knight (kn 2) [_moveP (kn 2) 1]) |> toBe Board.Unreachable);
+        expect (Board.turn Board.Knight (kn 2) [moveP (kn 2) 1]) |> toBe Board.Unreachable);
     );
   describe "Game play" (fun () ->
       test "[K-K2] fails" (fun () ->
@@ -84,11 +84,11 @@ let () =
     );
   describe "Vertical movement" (fun () ->
       test "moves pawn from P-K2 to P-K3" (fun () ->
-          expect(_moveP (k 2) 1) |> toEqual (Board.Pawn, ((k 2), []), (k 3)));
+          expect(moveP (k 2) 1) |> toEqual (Board.Pawn, ((k 2), []), (k 3)));
       test "keeps pawn in K2 when the steps count is 0" (fun () ->
-          expect(_moveP (k 2) 0) |> toEqual (Board.Pawn, ((k 2), []), (k 2)));
+          expect(moveP (k 2) 0) |> toEqual (Board.Pawn, ((k 2), []), (k 2)));
       test "moves queen one step forward" (fun() ->
-          expect(_moveQ (q 2) 1) |> toEqual (Board.Queen, ((q 2), []), (q 3)));
+          expect(moveQ (q 2) 1) |> toEqual (Board.Queen, ((q 2), []), (q 3)));
       test "moves queen one step backward" (fun() ->
-          expect(_moveQ (q 2) (-1)) |> toEqual (Board.Queen, ((q 2), []), (q 1)));
+          expect(moveQ (q 2) (-1)) |> toEqual (Board.Queen, ((q 2), []), (q 1)));
     );
