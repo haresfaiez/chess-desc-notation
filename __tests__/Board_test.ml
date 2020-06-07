b36ac499f8f0d77e7821213daf122ae47eb985bb
faiez <felhares@iobeya.com>
2020-06-07 11:59:25 +0100

    Use turn helper to check Board.turn in tests

--- __tests__/Board_test.ml
+++ __tests__/Board_test.ml
@@ -47,7 +47,7 @@ let () =
       test "-K3 is unreachable from K1" (fun () ->
           expect (Board.move [toPos (k 1)] (k 3)) |> toBe Board.Unreachable);
       test "-K7 is unreachable from K2" (fun () ->
-          expect (Board.turn [toPos (k 2)] (k 3) [(Board.King, (toPos (k 1)), (toPos (k 2)))]) |> toBe Board.Unreachable);
+          expect (turn Board.King (k 3) [(Board.King, (toPos (k 1)), (toPos (k 2)))]) |> toBe Board.Unreachable);
     );
   describe "Movement: Q" (fun () ->
       test "-Q4 succeeds from Q1" (fun () ->
