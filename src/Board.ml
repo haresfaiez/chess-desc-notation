0b18ab2f11da45e5d31fbc6624861ad47ff38c04
faiez <felhares@iobeya.com>
2020-06-07 11:37:04 +0100

    Move handling of terminal cases from Board.turn to Board._turn

--- src/Board.ml
+++ src/Board.ml
@@ -55,13 +55,15 @@ module Board = struct
   let _turn sources destination history =
     match history with
     | []                                           -> move sources destination
+    | (_, _, (dst, _)) :: _ when destination = dst -> Conflict
+    | (_, (src, _), _) :: _ when destination = src -> move sources destination
   
   let rec turn piece destination history = (* TODO: replace piece with sources *)
     let sources = setup piece in
     match history with
     | []                                           -> _turn sources destination history
-    | (_, _, (dst, _)) :: _ when destination = dst -> Conflict
-    | (_, (src, _), _) :: _ when destination = src -> move sources destination
+    | (_, _, (dst, _)) :: _ when destination = dst -> _turn sources destination history
+    | (_, (src, _), _) :: _ when destination = src -> _turn sources destination history
     | _                                            -> turn piece destination (List.tl history)
 
   let rec play moves history =
